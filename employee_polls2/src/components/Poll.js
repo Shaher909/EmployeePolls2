53949b732d75dfdbe714314de01aa66765da1902
shaher.je <shaher.jamaleldin@gmail.com>
2024-01-04 05:02:24 +0100

    includes chosen option into the Poll component and the number / % of votes for each option

--- employee_polls2/src/components/Poll.js
+++ employee_polls2/src/components/Poll.js
@@ -1,20 +1,41 @@
+import React from "react";
 import Title from "./Title";
 import { connect } from 'react-redux';
 
-
-const Poll = ({ question, author, authedUser }) => {
+const Poll = ({ question, author, authedUser, userAnswers }) => {
     if (!question) {
-      return <p>Question not found</p>;
+        return <p>Question not found</p>;
     }
-  
+
     const optionOneText = question.optionOne.text;
     const optionTwoText = question.optionTwo.text;
-  
     const authorName = author.name;
-  
+
     // Assuming the avatar URL is stored in the users object based on the user ID
     const authorAvatarURL = author.avatarURL;
 
+    // Check if the user has answered the current question
+    const hasAnswered = authedUser && userAnswers && userAnswers[question.id];
+
+    // Function to handle the vote (you can implement the logic based on your requirements)
+    const handleVote = (option) => {
+        // Implement your vote logic here
+        console.log(`Voting for ${option}`);
+    };
+
+    // Calculate the number of votes for each option
+    const votesOptionOne = question.optionOne.votes.length;
+    const votesOptionTwo = question.optionTwo.votes.length;
+
+    // Calculate the total number of votes
+    const totalVotes = votesOptionOne + votesOptionTwo;
+
+    // Calculate the percentage of votes for each option
+    const percentageOptionOne = (votesOptionOne / totalVotes) * 100;
+    const percentageOptionTwo = (votesOptionTwo / totalVotes) * 100;
+
+    // Check if the authedUser has answered this question and get the chosen option
+    const chosenOption = hasAnswered ? userAnswers[question.id] : null;
 
     return (
         <div>
@@ -25,21 +46,22 @@ const Poll = ({ question, author, authedUser }) => {
                 <img src={authorAvatarURL} alt={`${authorName}'s Avatar`} className="bigAvatar" />
             </div>
             <div className="questionOptions">
-                <div className="Option">
+                <div className={`Option ${chosenOption === 'optionOne' ? 'chosenOption' : ''}`}>
                     <p>{optionOneText}</p>
-                    <button>Vote</button>
+                    <button disabled={hasAnswered} onClick={() => handleVote("optionOne")}>
+                        {hasAnswered ? `Voted - ${votesOptionOne} votes (${percentageOptionOne.toFixed(2)}%)` : "Vote"}
+                    </button>
                 </div>
-                <div className="Option">
+                <div className={`Option ${chosenOption === 'optionTwo' ? 'chosenOption' : ''}`}>
                     <p>{optionTwoText}</p>
-                    <button>Vote</button>
+                    <button disabled={hasAnswered} onClick={() => handleVote("optionTwo")}>
+                        {hasAnswered ? `Voted - ${votesOptionTwo} votes (${percentageOptionTwo.toFixed(2)}%)` : "Vote"}
+                    </button>
                 </div>
             </div>
-
-            
         </div>
-    ); 
+    );
 }
-
 
 const mapStateToProps = (state) => {
     const selectedQuestionID = state.selectedQuestion.selectedQuestionID;
@@ -47,12 +69,14 @@ const mapStateToProps = (state) => {
     const authorID = question ? question.author : null;
     const author = authorID ? state.users[authorID] : null;
     const authedUser = state.authedUser;
-  
+    const userAnswers = authedUser ? state.users[authedUser].answers : null;
+
     return {
-      question,
-      author,
-      authedUser,
+        question,
+        author,
+        authedUser,
+        userAnswers,
     };
-  };
-  
-  export default connect(mapStateToProps)(Poll);
+};
+
+export default connect(mapStateToProps)(Poll);
