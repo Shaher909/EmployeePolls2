aac0c57b70b16c2f7cba01588768714a30771d41
shaher.je <shaher.jamaleldin@gmail.com>
2024-01-06 20:35:20 +0100

    introduces password validation, jest and some layout improvements

--- employee_polls2/src/components/Login.js
+++ employee_polls2/src/components/Login.js
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import { connect } from 'react-redux';
 import { setAuthUser } from '../actions/authedUser';
@@ -7,50 +6,59 @@ import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 const Login = ({ users, dispatch }) => {
   const [selectedUser, setSelectedUser] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate(); // Initialize the navigate hook
 
   const handleLogin = async () => {
-    if (selectedUser) {
+    if (selectedUser && password && users[selectedUser]?.password === password) {
       // Dispatch setAuthUser with the user ID
       dispatch(setAuthUser(selectedUser));
 
       // Redirect to dashboard after successful login
       navigate('/dashboard');
-
     } else {
-      // Handle case when no user is selected
-      console.log('Please select a user.');
+      alert('Invalid username or password');
+      // Handle case when no user is selected or password is incorrect
+      console.log('Invalid username or password. Please try again.');
     }
   };
 
-    return (
-        <div>
-            <Title text={"Employees Polls Portal - Login Page"} />
-            <label htmlFor="dropdown">Username:</label>
-            <br />
-            <select
-                id="dropdown"
-                onChange={(e) => setSelectedUser(e.target.value)}
-                value={selectedUser}
-            >
-                <option value="" disabled>
-                Select a username
-                </option>
-                {Object.keys(users).map((userId) => (
-                <option key={userId} value={userId}>
-                    {users[userId].name}
-                </option>
-                ))}
-            </select>
-            <br />
-            <input type="button" className="btn" value="Login" onClick={handleLogin} />
-        </div>
-    ); 
+  return (
+    <div className="formContainer">
+      <Title text={"Employees Polls Portal - Login Page"} />
+      <label htmlFor="dropdown">Username:</label>
+      <br />
+      <select
+        id="dropdown"
+        onChange={(e) => setSelectedUser(e.target.value)}
+        value={selectedUser}
+      >
+        <option value="" disabled>
+          Select a username
+        </option>
+        {Object.keys(users).map((userId) => (
+          <option key={userId} value={userId}>
+            {users[userId].name}
+          </option>
+        ))}
+      </select>
+      <br />
+      <label htmlFor="password">Password:</label>
+      <br />
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <input type="button" className="btn" value="Login" onClick={handleLogin} />
+    </div>
+  );
 }
 
 const mapStateToProps = ({ users }) => ({
-    users,
-  });
-  
-  export default connect(mapStateToProps)(Login)
+  users,
+});
 
+export default connect(mapStateToProps)(Login);
