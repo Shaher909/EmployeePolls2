d929e8ada3d5b398329d96ba9940dfb87ef4207b
shaher.je <shaher.jamaleldin@gmail.com>
2024-01-10 20:55:46 +0100

    introduces CSS improvements and TAB feature on the dashboard page to separate open/done polls from UI perspective

--- employee_polls2/src/components/Dashboard.js
+++ employee_polls2/src/components/Dashboard.js
@@ -1,18 +1,34 @@
+import React, { useState } from 'react';
 import Title from "./Title";
 import PollsCollection from "./PollsCollection";
+import '../App.css';
 
 const Dashboard = () => {
+  const [activeTab, setActiveTab] = useState('new');
 
-    return (
-        <div>
-            <Title text={"Polls Dashboard"} />
-            <PollsCollection title={"New Questions"} questionsAnswered={0}></PollsCollection>
-            <PollsCollection title={"Done"} questionsAnswered={1}></PollsCollection>
-        </div>
-    ); 
-}
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div>
+      <Title text={"Polls Dashboard"} />
+
+      {/* Tab Buttons */}
+      <div className="tab-buttons">
+        <button className={`tab-button ${activeTab === 'new' ? 'active' : ''}`} onClick={() => handleTabChange('new')}>
+          New Questions
+        </button>
+        <button className={`tab-button ${activeTab === 'done' ? 'active' : ''}`} onClick={() => handleTabChange('done')}>
+          Done
+        </button>
+      </div>
+
+      {/* Display PollsCollection based on activeTab */}
+      {activeTab === 'new' && <PollsCollection title={"New Questions"} questionsAnswered={0} />}
+      {activeTab === 'done' && <PollsCollection title={"Done"} questionsAnswered={1} />}
+    </div>
+  );
+};
 
 export default Dashboard;
-
-
-
